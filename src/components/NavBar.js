cf257cdf96e193db852f9066fa2b718892b9ee00
Harshit Anand <harshitanand97@gmail.com>
2022-03-29 11:15:08 +0530

    Made wishlist notification working

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -27,8 +27,10 @@ function NavBar(){
                     <Link to="/cart" className="stacked-link" id="cart"><i className="bi bi-cart"></i></Link>
                 </li>
                 <li className="menu notif cart-notif"><span>{cartState.count}</span></li>
-                <li className="menu"><a href="" id="wishlist"><i className="bi bi-heart"></i></a></li>
-                <li className="menu notif wishlist-notif"><span>0</span></li>
+                <li className="menu">
+                    <Link to="/wishlist" className="stacked-link" id="wishlist"><i className="bi bi-heart"></i></Link>
+                </li>
+                <li className="menu notif wishlist-notif"><span>{cartState.wishlist.length}</span></li>
             </ul>
             <ul className="menus">
                 <li className="menu"><Link to="/" className="stacked-link">Home</Link></li>
