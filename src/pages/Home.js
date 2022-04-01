3d415a6d802330cefd8850c06916ae62e7c8f0df
Harshit Anand <harshitanand97@gmail.com>
2022-04-01 11:07:40 +0530

    Change home page content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,13 +7,12 @@ function Home() {
     <section className="home-container">
       <div className="image-wrapper">
         <img
-          src="https://images.unsplash.com/photo-1542744095-291d1f67b221?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          src="https://cdn.printnetwork.com/production/assets/5966561450122033bd4456f8/imageLocker/blog-description/blog/sales_banners.jpg"
           alt=""
           className="home-image"
         />
       </div>
       <div className="overlay-text">
-        <h1 className="overlay-text__title">All your IT Products at one Place</h1>
         <Link to="/pl"><button className="btn btn-secondary">Shop Now</button></Link>
       </div>
     </section>
